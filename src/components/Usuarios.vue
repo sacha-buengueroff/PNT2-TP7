95bdf3aa3b7e4e5396d4e0af4a4cb085929a7dcc
sacha-buengueroff <sachabuengue@gmail.com>
2022-12-04 18:24:29 -0300

    first commit

---
+++ src/components/Usuarios.vue
@@ -0,0 +1,49 @@
+<template>
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th scope="col">Nombre</th>
+          <th scope="col">Edad</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in users" :key="index">
+          <td>{{ user.nombre }}</td>
+          <td>{{ user.edad }}</td>
+          <td>{{ user.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+  name: "src-components-usuarios",
+  props: [],
+  beforeMount() {
+    this.getUsers()
+  },
+  data() {
+    return {
+      url: "https://633f79bd0dbc3309f3cb1986.mockapi.io/api/usuariosTP7",
+      users: []
+    };
+  },
+  methods: {
+    async getUsers() {
+      try {
+        let {data: users} = await this.axios(this.url)
+        this.users = users
+      }
+      catch(error) {
+        console.log(error)
+      }
+    }
+  },
+  computed: {},
+};
+</script>
+
+<style scoped lang="css">
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import './axios'
+import './bootstrap'
+import {router} from './router'
+import './form'
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
